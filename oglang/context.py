c56140b96eefaed82bd5a376c3ea44c6f46527ad
Miles Macklin <miles.macklin@gmail.com>
2021-04-14 10:44:29 +1200

    Clean up ScopedTimer, add support for recording timer dicts

--- oglang/context.py
+++ oglang/context.py
@@ -10,83 +10,10 @@ from ctypes import*
 
 
 from oglang.types import *
+from oglang.utils import *
 
 import oglang.codegen
 import oglang.cuda
-
-class ScopedTimer:
-
-    indent = -1
-
-    enabled = True
-
-    def __init__(self, name, active=True, detailed=False):
-        self.name = name
-        self.active = active and self.enabled
-        self.detailed = detailed
-
-    def __enter__(self):
-
-        if (self.active):
-            self.start = timeit.default_timer()
-            ScopedTimer.indent += 1
-
-            if (self.detailed):
-                self.cp = cProfile.Profile()
-                self.cp.clear()
-                self.cp.enable()
-
-
-    def __exit__(self, exc_type, exc_value, traceback):
-
-        if (self.detailed):
-            self.cp.disable()
-            self.cp.print_stats(sort='tottime')
-
-        if (self.active):
-            elapsed = (timeit.default_timer() - self.start) * 1000.0
-
-            indent = ""
-            for i in range(ScopedTimer.indent):
-                indent += "\t"
-
-            print("{}{} took {:.2f} ms".format(indent, self.name, elapsed))
-
-            ScopedTimer.indent -= 1
-
-
-# runs vcvars and copies back the build environment
-def set_build_env():
-
-    def find_vcvars_path():
-        import glob
-        for edition in ['Enterprise', 'Professional', 'BuildTools', 'Community']:
-            paths = sorted(glob.glob(r"C:\Program Files (x86)\Microsoft Visual Studio\*\%s\VC\Auxiliary\Build\vcvars64.bat" % edition), reverse=True)
-            if paths:
-                return paths[0]
-
-    if os.name == 'nt':
-
-        vcvars_path = find_vcvars_path()
-
-        # merge vcvars with our env
-        s = '"{}" && set'.format(vcvars_path)
-        output = os.popen(s).read()
-        for line in output.splitlines():
-            pair = line.split("=", 1)
-            if (len(pair) >= 2):
-                os.environ[pair[0]] = pair[1]
-
-
-
-# See PyTorch for reference on how to find nvcc.exe more robustly, https://pytorch.org/docs/stable/_modules/torch/utils/cpp_extension.html#CppExtension
-def find_cuda():
-    
-    # Guess #1
-    cuda_home = os.environ.get('CUDA_HOME') or os.environ.get('CUDA_PATH')
-    return cuda_home
-    
-
 
 
 # represents either a built-in or user-defined function
@@ -1011,6 +938,10 @@ def copy(src, dest):
 
     elif (src.device == "cuda" and dest.device == "cpu"):
         oglang.cuda.cuMemcpyDtoHAsync(c_void_p(dest.data), c_void_p(src.data), c_size_t(src.capacity), c_void_p(0))     # todo, just copy active region not full capacity
+
+    elif (src.device == "cpu" and dest.device == "cpu"):
+        runtime.crt.memcpy(c_void_p(dest.data), c_void_p(src.data), c_size_t(src.capacity))
+
     
 
 def empty(n, dtype=float, device="cpu"):
--- oglang/utils.py
+++ oglang/utils.py
@@ -1,8 +1,8 @@
+import os
 import timeit
 import cProfile
 
 # timer utils
-
 
 class ScopedTimer:
 
@@ -10,10 +10,15 @@ class ScopedTimer:
 
     enabled = True
 
-    def __init__(self, name, active=True, detailed=False):
+    def __init__(self, name, active=True, detailed=False, dict=None):
         self.name = name
         self.active = active and self.enabled
         self.detailed = detailed
+        self.dict = dict
+
+        if self.dict is not None:
+            if name not in self.dict:
+                self.dict[name] = []
 
     def __enter__(self):
 
@@ -34,13 +39,50 @@ class ScopedTimer:
             self.cp.print_stats(sort='tottime')
 
         if (self.active):
-            elapsed = (timeit.default_timer() - self.start) * 1000.0
+            self.elapsed = (timeit.default_timer() - self.start) * 1000.0
+
+            if self.dict is not None:
+                self.dict[self.name].append(self.elapsed)
 
             indent = ""
             for i in range(ScopedTimer.indent):
                 indent += "\t"
 
-            print("{}{} took {:.2f} ms".format(indent, self.name, elapsed))
+            print("{}{} took {:.2f} ms".format(indent, self.name, self.elapsed))
 
             ScopedTimer.indent -= 1
 
+        
+
+
+# runs vcvars and copies back the build environment
+def set_build_env():
+
+    def find_vcvars_path():
+        import glob
+        for edition in ['Enterprise', 'Professional', 'BuildTools', 'Community']:
+            paths = sorted(glob.glob(r"C:\Program Files (x86)\Microsoft Visual Studio\*\%s\VC\Auxiliary\Build\vcvars64.bat" % edition), reverse=True)
+            if paths:
+                return paths[0]
+
+    if os.name == 'nt':
+
+        vcvars_path = find_vcvars_path()
+
+        # merge vcvars with our env
+        s = '"{}" && set'.format(vcvars_path)
+        output = os.popen(s).read()
+        for line in output.splitlines():
+            pair = line.split("=", 1)
+            if (len(pair) >= 2):
+                os.environ[pair[0]] = pair[1]
+
+
+
+# See PyTorch for reference on how to find nvcc.exe more robustly, https://pytorch.org/docs/stable/_modules/torch/utils/cpp_extension.html#CppExtension
+def find_cuda():
+    
+    # Guess #1
+    cuda_home = os.environ.get('CUDA_HOME') or os.environ.get('CUDA_PATH')
+    return cuda_home
+    
