4e9d07344f1f607e5cdc330743d85082f1022048
Miles Macklin <mmacklin@nvidia.com>
2021-09-22 10:18:25 +1200

    Set WinSDK dir

--- build.bat
+++ build.bat
@@ -6,4 +6,7 @@ SET PYTHON="%~dp0\_build\target-deps\python\python.exe"
 REM Dependencies
 call %PYTHON% -m pip install numpy
 
+REM WinSDK
+SET WindowsSDKDir="%~dp0\_build\host-deps\winsdk"
+
 call %PYTHON% build_lib.py
