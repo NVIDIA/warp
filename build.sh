7e35eaf4b721820092084147ae0005e01676e096
Miles Macklin <mmacklin@nvidia.com>
2021-09-22 15:04:40 +1200

    Linux build

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
 #source "$SCRIPT_DIR/repo.sh" build --fetch-only $@ || exit $?
 
-"$SCRIPT_DIR/repo.sh" build --fetch-only $@ 
+"$SCRIPT_DIR/repo.sh" build --fetch-only --no-docker $@ 
+
+python --version
+python3 --version
 
 # pip deps
 python -m pip install numpy
