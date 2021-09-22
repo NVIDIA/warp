c60ec4ca5b0339f0f8205afc3cc2550b3c423c37
Miles Macklin <mmacklin@nvidia.com>
2021-09-22 14:48:59 +1200

    Linx build update

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
-source "$SCRIPT_DIR/repo.sh" build --fetch-only --no-docker $@ || exit $?
+#source "$SCRIPT_DIR/repo.sh" build --fetch-only $@ || exit $?
+source "$SCRIPT_DIR/repo.sh" build --fetch-only $@ 
 
 # pip deps
 python -m pip install numpy
