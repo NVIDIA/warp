04800df2501eac03d9181f20ffff7c1840d82509
Miles Macklin <mmacklin@nvidia.com>
2021-09-23 12:13:19 +1200

    Slash fix

--- test.bat
+++ test.bat
@@ -5,12 +5,12 @@ SET PYTHON="%~dp0\_build\target-deps\python\python.exe"
 
 dir
 dir _build
-dir _build/packages
+dir _build\packages
 
-cd _build/packages
+cd _build\packages
 
 echo "Installing Warp to Python"
 call %PYTHON% -m pip install -e .
 
 echo "Running tests"
-call %PYTHON% tests/test_ctypes.py
+call %PYTHON% tests\test_ctypes.py
