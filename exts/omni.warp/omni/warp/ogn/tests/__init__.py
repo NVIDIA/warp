b2592b304c2c5cfa16fb49bab63047145729cf7b
Miles Macklin <mmacklin@nvidia.com>
2021-11-02 13:00:58 +1300

    Update example ripple files

--- exts/omni.warp/omni/warp/ogn/tests/__init__.py
+++ exts/omni.warp/omni/warp/ogn/tests/__init__.py
@@ -1,5 +1,3 @@
 from .TestOgnCloth import TestOgnCloth
 from .TestOgnFixedTime import TestOgnFixedTime
-from .TestOgnRecordPrimAttribute import TestOgnRecordPrimAttribute
 from .TestOgnRipple import TestOgnRipple
-from .TestOgnSetAttribute import TestOgnSetAttribute
