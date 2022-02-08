7f06abdae2c0711e578ee52deced2c2210a1d029
daedalus5 <zach.corse@gmail.com>
2022-02-08 17:21:41 +0000

    snake_case fix

--- warp/native/noise.h
+++ warp/native/noise.h
@@ -27,13 +27,13 @@ inline CUDA_CALLABLE float interpolate_gradient(float a0, float a1, float t, flo
     return (d_a1 - d_a0) * smootherstep(t) + (a1 - a0) * smootherstep_gradient(t) * d_t + d_a0;
 }
 
-inline CUDA_CALLABLE float random_gradient_1D(uint32 seed, int ix)
+inline CUDA_CALLABLE float random_gradient_1d(uint32 seed, int ix)
 {
     uint32 state = seed + uint32(ix);
     return randf(state, -1.f, 1.f);
 }
 
-inline CUDA_CALLABLE vec2 random_gradient_2D(uint32 seed, int ix, int iy, int px)
+inline CUDA_CALLABLE vec2 random_gradient_2d(uint32 seed, int ix, int iy, int px)
 {
     int idx = ix + px * iy;
     uint32 state = seed + uint32(idx);
@@ -43,7 +43,7 @@ inline CUDA_CALLABLE vec2 random_gradient_2D(uint32 seed, int ix, int iy, int px
     return vec2(x, y);
 }
 
-inline CUDA_CALLABLE vec3 random_gradient_3D(uint32 seed, int ix, int iy, int iz, int px, int py)
+inline CUDA_CALLABLE vec3 random_gradient_3d(uint32 seed, int ix, int iy, int iz, int px, int py)
 {
     int idx = ix + px * (iy + py * iz);
     uint32 state = seed + uint32(idx);
@@ -55,7 +55,7 @@ inline CUDA_CALLABLE vec3 random_gradient_3D(uint32 seed, int ix, int iy, int iz
     return vec3(x, y, z);
 }
 
-inline CUDA_CALLABLE vec4 random_gradient_4D(uint32 seed, int ix, int iy, int iz, int it, int px, int py, int pz)
+inline CUDA_CALLABLE vec4 random_gradient_4d(uint32 seed, int ix, int iy, int iz, int it, int px, int py, int pz)
 {
     int idx = ix + px * (iy + py * (iz + it * pz));
     uint32 state = seed + uint32(idx);
@@ -69,51 +69,51 @@ inline CUDA_CALLABLE vec4 random_gradient_4D(uint32 seed, int ix, int iy, int iz
     return vec4(x, y, z, t);
 }
 
-inline CUDA_CALLABLE float dot_grid_gradient_1D(uint32 seed, int ix, float dx)
+inline CUDA_CALLABLE float dot_grid_gradient_1d(uint32 seed, int ix, float dx)
 {
-    float gradient = random_gradient_1D(seed, ix);
+    float gradient = random_gradient_1d(seed, ix);
     return dx*gradient;
 }
 
-inline CUDA_CALLABLE float dot_grid_gradient_1D_gradient(uint32 seed, int ix, float d_dx)
+inline CUDA_CALLABLE float dot_grid_gradient_1d_gradient(uint32 seed, int ix, float d_dx)
 {
-    float gradient = random_gradient_1D(seed, ix);
+    float gradient = random_gradient_1d(seed, ix);
     return d_dx*gradient;
 }
 
-inline CUDA_CALLABLE float dot_grid_gradient_2D(uint32 seed, int ix, int iy, float dx, float dy, int px)
+inline CUDA_CALLABLE float dot_grid_gradient_2d(uint32 seed, int ix, int iy, float dx, float dy, int px)
 {
-    vec2 gradient = random_gradient_2D(seed, ix, iy, px);
+    vec2 gradient = random_gradient_2d(seed, ix, iy, px);
     return (dx*gradient.x + dy*gradient.y);
 }
 
-inline CUDA_CALLABLE float dot_grid_gradient_2D_gradient(uint32 seed, int ix, int iy, float d_dx, float d_dy, int px)
+inline CUDA_CALLABLE float dot_grid_gradient_2d_gradient(uint32 seed, int ix, int iy, float d_dx, float d_dy, int px)
 {
-    vec2 gradient = random_gradient_2D(seed, ix, iy, px);
+    vec2 gradient = random_gradient_2d(seed, ix, iy, px);
     return (d_dx*gradient.x + d_dy*gradient.y);
 }
 
-inline CUDA_CALLABLE float dot_grid_gradient_3D(uint32 seed, int ix, int iy, int iz, float dx, float dy, float dz, int px, int py)
+inline CUDA_CALLABLE float dot_grid_gradient_3d(uint32 seed, int ix, int iy, int iz, float dx, float dy, float dz, int px, int py)
 {
-    vec3 gradient = random_gradient_3D(seed, ix, iy, iz, px, py);
+    vec3 gradient = random_gradient_3d(seed, ix, iy, iz, px, py);
     return (dx*gradient.x + dy*gradient.y + dz*gradient.z);
 }
 
-inline CUDA_CALLABLE float dot_grid_gradient_3D_gradient(uint32 seed, int ix, int iy, int iz, float d_dx, float d_dy, float d_dz, int px, int py)
+inline CUDA_CALLABLE float dot_grid_gradient_3d_gradient(uint32 seed, int ix, int iy, int iz, float d_dx, float d_dy, float d_dz, int px, int py)
 {
-    vec3 gradient = random_gradient_3D(seed, ix, iy, iz, px, py);
+    vec3 gradient = random_gradient_3d(seed, ix, iy, iz, px, py);
     return (d_dx*gradient.x + d_dy*gradient.y + d_dz*gradient.z);
 }
 
-inline CUDA_CALLABLE float dot_grid_gradient_4D(uint32 seed, int ix, int iy, int iz, int it, float dx, float dy, float dz, float dt, int px, int py, int pz)
+inline CUDA_CALLABLE float dot_grid_gradient_4d(uint32 seed, int ix, int iy, int iz, int it, float dx, float dy, float dz, float dt, int px, int py, int pz)
 {
-    vec4 gradient = random_gradient_4D(seed, ix, iy, iz, it, px, py, pz);
+    vec4 gradient = random_gradient_4d(seed, ix, iy, iz, it, px, py, pz);
     return (dx*gradient.x + dy*gradient.y + dz*gradient.z + dt*gradient.w);
 }
 
-inline CUDA_CALLABLE float dot_grid_gradient_4D_gradient(uint32 seed, int ix, int iy, int iz, int it, float d_dx, float d_dy, float d_dz, float d_dt, int px, int py, int pz)
+inline CUDA_CALLABLE float dot_grid_gradient_4d_gradient(uint32 seed, int ix, int iy, int iz, int it, float d_dx, float d_dy, float d_dz, float d_dt, int px, int py, int pz)
 {
-    vec4 gradient = random_gradient_4D(seed, ix, iy, iz, it, px, py, pz);
+    vec4 gradient = random_gradient_4d(seed, ix, iy, iz, it, px, py, pz);
     return (d_dx*gradient.x + d_dy*gradient.y + d_dz*gradient.z + d_dt*gradient.w);    
 }
 
@@ -124,8 +124,8 @@ inline CUDA_CALLABLE float pnoise(uint32 seed, float x, int px)
     int x0 = mod(((int)floor(x)), px);
     int x1 = mod((x0 + 1), px);
 
-    float v0 = dot_grid_gradient_1D(seed, x0, dx);
-    float v1 = dot_grid_gradient_1D(seed, x1, dx-1.f);
+    float v0 = dot_grid_gradient_1d(seed, x0, dx);
+    float v1 = dot_grid_gradient_1d(seed, x1, dx-1.f);
 
     return interpolate(v0, v1, dx);
 }
@@ -140,11 +140,11 @@ inline CUDA_CALLABLE void adj_pnoise(uint32 seed, float x, int px, uint32& adj_s
     int x0 = mod(((int)floor(x)), px);
     int x1 = mod((x0 + 1), px);
 
-    float v0 = dot_grid_gradient_1D(seed, x0, dx);
-    float v1 = dot_grid_gradient_1D(seed, x1, dx-1.f);
+    float v0 = dot_grid_gradient_1d(seed, x0, dx);
+    float v1 = dot_grid_gradient_1d(seed, x1, dx-1.f);
 
-    float d_v0_dx = dot_grid_gradient_1D_gradient(seed, x0, heaviside_x);
-    float d_v1_dx = dot_grid_gradient_1D_gradient(seed, x1, heaviside_x);
+    float d_v0_dx = dot_grid_gradient_1d_gradient(seed, x0, heaviside_x);
+    float d_v1_dx = dot_grid_gradient_1d_gradient(seed, x1, heaviside_x);
 
     adj_x += interpolate_gradient(v0, v1, dx, d_v0_dx, d_v1_dx, heaviside_x) * adj_ret;
 }
@@ -161,12 +161,12 @@ inline CUDA_CALLABLE float pnoise(uint32 seed, const vec2& xy, int px, int py)
     int y1 = mod((y0 + 1), py);
 
     //vXY
-    float v00 = dot_grid_gradient_2D(seed, x0, y0, dx, dy, px);
-    float v10 = dot_grid_gradient_2D(seed, x1, y0, dx-1.f, dy, px);
+    float v00 = dot_grid_gradient_2d(seed, x0, y0, dx, dy, px);
+    float v10 = dot_grid_gradient_2d(seed, x1, y0, dx-1.f, dy, px);
     float xi0 = interpolate(v00, v10, dx);
 
-    float v01 = dot_grid_gradient_2D(seed, x0, y1, dx, dy-1.f, px);
-    float v11 = dot_grid_gradient_2D(seed, x1, y1, dx-1.f, dy-1.f, px);
+    float v01 = dot_grid_gradient_2d(seed, x0, y1, dx, dy-1.f, px);
+    float v11 = dot_grid_gradient_2d(seed, x1, y1, dx-1.f, dy-1.f, px);
     float xi1 = interpolate(v01, v11, dx);
 
     return interpolate(xi0, xi1, dy);
@@ -188,28 +188,28 @@ inline CUDA_CALLABLE void adj_pnoise(uint32 seed, const vec2& xy, int px, int py
     int x1 = mod((x0 + 1), px);
     int y1 = mod((y0 + 1), py);
 
-    float v00 = dot_grid_gradient_2D(seed, x0, y0, dx, dy, px);
-    float v10 = dot_grid_gradient_2D(seed, x1, y0, dx-1.f, dy, px);
+    float v00 = dot_grid_gradient_2d(seed, x0, y0, dx, dy, px);
+    float v10 = dot_grid_gradient_2d(seed, x1, y0, dx-1.f, dy, px);
     float xi0 = interpolate(v00, v10, dx);
 
-    float v01 = dot_grid_gradient_2D(seed, x0, y1, dx, dy-1.f, px);
-    float v11 = dot_grid_gradient_2D(seed, x1, y1, dx-1.f, dy-1.f, px);
+    float v01 = dot_grid_gradient_2d(seed, x0, y1, dx, dy-1.f, px);
+    float v11 = dot_grid_gradient_2d(seed, x1, y1, dx-1.f, dy-1.f, px);
     float xi1 = interpolate(v01, v11, dx);
 
-    float d_v00_dx = dot_grid_gradient_2D_gradient(seed, x0, y0, heaviside_x, 0.f, px);
-    float d_v10_dx = dot_grid_gradient_2D_gradient(seed, x1, y0, heaviside_x, 0.f, px);
+    float d_v00_dx = dot_grid_gradient_2d_gradient(seed, x0, y0, heaviside_x, 0.f, px);
+    float d_v10_dx = dot_grid_gradient_2d_gradient(seed, x1, y0, heaviside_x, 0.f, px);
     float d_xi0_dx = interpolate_gradient(v00, v10, dx, d_v00_dx, d_v10_dx, heaviside_x);
 
-    float d_v01_dx = dot_grid_gradient_2D_gradient(seed, x0, y1, heaviside_x, 0.f, px);
-    float d_v11_dx = dot_grid_gradient_2D_gradient(seed, x1, y1, heaviside_x, 0.f, px);
+    float d_v01_dx = dot_grid_gradient_2d_gradient(seed, x0, y1, heaviside_x, 0.f, px);
+    float d_v11_dx = dot_grid_gradient_2d_gradient(seed, x1, y1, heaviside_x, 0.f, px);
     float d_xi1_dx = interpolate_gradient(v01, v11, dx, d_v01_dx, d_v11_dx, heaviside_x);
     
-    float d_v00_dy = dot_grid_gradient_2D_gradient(seed, x0, y0, 0.0, heaviside_y, px);
-    float d_v10_dy = dot_grid_gradient_2D_gradient(seed, x1, y0, 0.0, heaviside_y, px);
+    float d_v00_dy = dot_grid_gradient_2d_gradient(seed, x0, y0, 0.0, heaviside_y, px);
+    float d_v10_dy = dot_grid_gradient_2d_gradient(seed, x1, y0, 0.0, heaviside_y, px);
     float d_xi0_dy = interpolate_gradient(v00, v10, dx, d_v00_dy, d_v10_dy, 0.0);
 
-    float d_v01_dy = dot_grid_gradient_2D_gradient(seed, x0, y1, 0.0, heaviside_y, px);
-    float d_v11_dy = dot_grid_gradient_2D_gradient(seed, x1, y1, 0.0, heaviside_y, px);
+    float d_v01_dy = dot_grid_gradient_2d_gradient(seed, x0, y1, 0.0, heaviside_y, px);
+    float d_v11_dy = dot_grid_gradient_2d_gradient(seed, x1, y1, 0.0, heaviside_y, px);
     float d_xi1_dy = interpolate_gradient(v01, v11, dx, d_v01_dy, d_v11_dy, 0.0);
 
     adj_xy.x += interpolate_gradient(xi0, xi1, dy, d_xi0_dx, d_xi1_dx, 0.0) * adj_ret;
@@ -231,22 +231,22 @@ inline CUDA_CALLABLE float pnoise(uint32 seed, const vec3& xyz, int px, int py, 
     int z1 = mod((z0 + 1), pz);
 
     //vXYZ
-    float v000 = dot_grid_gradient_3D(seed, x0, y0, z0, dx, dy, dz, px, py);
-    float v100 = dot_grid_gradient_3D(seed, x1, y0, z0, dx-1.f, dy, dz, px, py);
+    float v000 = dot_grid_gradient_3d(seed, x0, y0, z0, dx, dy, dz, px, py);
+    float v100 = dot_grid_gradient_3d(seed, x1, y0, z0, dx-1.f, dy, dz, px, py);
     float xi00 = interpolate(v000, v100, dx);
 
-    float v010 = dot_grid_gradient_3D(seed, x0, y1, z0, dx, dy-1.f, dz, px, py);
-    float v110 = dot_grid_gradient_3D(seed, x1, y1, z0, dx-1.f, dy-1.f, dz, px, py);
+    float v010 = dot_grid_gradient_3d(seed, x0, y1, z0, dx, dy-1.f, dz, px, py);
+    float v110 = dot_grid_gradient_3d(seed, x1, y1, z0, dx-1.f, dy-1.f, dz, px, py);
     float xi10 = interpolate(v010, v110, dx);
 
     float yi0 = interpolate(xi00, xi10, dy);
 
-    float v001 = dot_grid_gradient_3D(seed, x0, y0, z1, dx, dy, dz-1.f, px, py);
-    float v101 = dot_grid_gradient_3D(seed, x1, y0, z1, dx-1.f, dy, dz-1.f, px, py);
+    float v001 = dot_grid_gradient_3d(seed, x0, y0, z1, dx, dy, dz-1.f, px, py);
+    float v101 = dot_grid_gradient_3d(seed, x1, y0, z1, dx-1.f, dy, dz-1.f, px, py);
     float xi01 = interpolate(v001, v101, dx);
 
-    float v011 = dot_grid_gradient_3D(seed, x0, y1, z1, dx, dy-1.f, dz-1.f, px, py);
-    float v111 = dot_grid_gradient_3D(seed, x1, y1, z1, dx-1.f, dy-1.f, dz-1.f, px, py);
+    float v011 = dot_grid_gradient_3d(seed, x0, y1, z1, dx, dy-1.f, dz-1.f, px, py);
+    float v111 = dot_grid_gradient_3d(seed, x1, y1, z1, dx-1.f, dy-1.f, dz-1.f, px, py);
     float xi11 = interpolate(v011, v111, dx);
 
     float yi1 = interpolate(xi01, xi11, dy);
@@ -274,44 +274,44 @@ inline CUDA_CALLABLE float pnoise(uint32 seed, const vec4& xyzt, int px, int py,
     int t1 = mod((t0 + 1), pt);
 
     //vXYZT
-    float v0000 = dot_grid_gradient_4D(seed, x0, y0, z0, t0, dx, dy, dz, dt, px, py, pz);
-    float v1000 = dot_grid_gradient_4D(seed, x1, y0, z0, t0, dx-1.f, dy, dz, dt, px, py, pz);
+    float v0000 = dot_grid_gradient_4d(seed, x0, y0, z0, t0, dx, dy, dz, dt, px, py, pz);
+    float v1000 = dot_grid_gradient_4d(seed, x1, y0, z0, t0, dx-1.f, dy, dz, dt, px, py, pz);
     float xi000 = interpolate(v0000, v1000, dx);
 
-    float v0100 = dot_grid_gradient_4D(seed, x0, y1, z0, t0, dx, dy-1.f, dz, dt, px, py, pz);
-    float v1100 = dot_grid_gradient_4D(seed, x1, y1, z0, t0, dx-1.f, dy-1.f, dz, dt, px, py, pz);
+    float v0100 = dot_grid_gradient_4d(seed, x0, y1, z0, t0, dx, dy-1.f, dz, dt, px, py, pz);
+    float v1100 = dot_grid_gradient_4d(seed, x1, y1, z0, t0, dx-1.f, dy-1.f, dz, dt, px, py, pz);
     float xi100 = interpolate(v0100, v1100, dx);
 
     float yi00 = interpolate(xi000, xi100, dy);
 
-    float v0010 = dot_grid_gradient_4D(seed, x0, y0, z1, t0, dx, dy, dz-1.f, dt, px, py, pz);
-    float v1010 = dot_grid_gradient_4D(seed, x1, y0, z1, t0, dx-1.f, dy, dz-1.f, dt, px, py, pz);
+    float v0010 = dot_grid_gradient_4d(seed, x0, y0, z1, t0, dx, dy, dz-1.f, dt, px, py, pz);
+    float v1010 = dot_grid_gradient_4d(seed, x1, y0, z1, t0, dx-1.f, dy, dz-1.f, dt, px, py, pz);
     float xi010 = interpolate(v0010, v1010, dx);
 
-    float v0110 = dot_grid_gradient_4D(seed, x0, y1, z1, t0, dx, dy-1.f, dz-1.f, dt, px, py, pz);
-    float v1110 = dot_grid_gradient_4D(seed, x1, y1, z1, t0, dx-1.f, dy-1.f, dz-1.f, dt, px, py, pz);
+    float v0110 = dot_grid_gradient_4d(seed, x0, y1, z1, t0, dx, dy-1.f, dz-1.f, dt, px, py, pz);
+    float v1110 = dot_grid_gradient_4d(seed, x1, y1, z1, t0, dx-1.f, dy-1.f, dz-1.f, dt, px, py, pz);
     float xi110 = interpolate(v0110, v1110, dx);
 
     float yi10 = interpolate(xi010, xi110, dy);
 
     float zi0 = interpolate(yi00, yi10, dz);
 
-    float v0001 = dot_grid_gradient_4D(seed, x0, y0, z0, t1, dx, dy, dz, dt-1.f, px, py, pz);
-    float v1001 = dot_grid_gradient_4D(seed, x1, y0, z0, t1, dx-1.f, dy, dz, dt-1.f, px, py, pz);
+    float v0001 = dot_grid_gradient_4d(seed, x0, y0, z0, t1, dx, dy, dz, dt-1.f, px, py, pz);
+    float v1001 = dot_grid_gradient_4d(seed, x1, y0, z0, t1, dx-1.f, dy, dz, dt-1.f, px, py, pz);
     float xi001 = interpolate(v0001, v1001, dx);
 
-    float v0101 = dot_grid_gradient_4D(seed, x0, y1, z0, t1, dx, dy-1.f, dz, dt-1.f, px, py, pz);
-    float v1101 = dot_grid_gradient_4D(seed, x1, y1, z0, t1, dx-1.f, dy-1.f, dz, dt-1.f, px, py, pz);
+    float v0101 = dot_grid_gradient_4d(seed, x0, y1, z0, t1, dx, dy-1.f, dz, dt-1.f, px, py, pz);
+    float v1101 = dot_grid_gradient_4d(seed, x1, y1, z0, t1, dx-1.f, dy-1.f, dz, dt-1.f, px, py, pz);
     float xi101 = interpolate(v0101, v1101, dx);
 
     float yi01 = interpolate(xi001, xi101, dy);
 
-    float v0011 = dot_grid_gradient_4D(seed, x0, y0, z1, t1, dx, dy, dz-1.f, dt-1.f, px, py, pz);
-    float v1011 = dot_grid_gradient_4D(seed, x1, y0, z1, t1, dx-1.f, dy, dz-1.f, dt-1.f, px, py, pz);
+    float v0011 = dot_grid_gradient_4d(seed, x0, y0, z1, t1, dx, dy, dz-1.f, dt-1.f, px, py, pz);
+    float v1011 = dot_grid_gradient_4d(seed, x1, y0, z1, t1, dx-1.f, dy, dz-1.f, dt-1.f, px, py, pz);
     float xi011 = interpolate(v0011, v1011, dx);
 
-    float v0111 = dot_grid_gradient_4D(seed, x0, y1, z1, t1, dx, dy-1.f, dz-1.f, dt-1.f, px, py, pz);
-    float v1111 = dot_grid_gradient_4D(seed, x1, y1, z1, t1, dx-1.f, dy-1.f, dz-1.f, dt-1.f, px, py, pz);
+    float v0111 = dot_grid_gradient_4d(seed, x0, y1, z1, t1, dx, dy-1.f, dz-1.f, dt-1.f, px, py, pz);
+    float v1111 = dot_grid_gradient_4d(seed, x1, y1, z1, t1, dx-1.f, dy-1.f, dz-1.f, dt-1.f, px, py, pz);
     float xi111 = interpolate(v0111, v1111, dx);
 
     float yi11 = interpolate(xi011, xi111, dy);
