e6adcee38d7a6e978a203570872d583ee918c35e
Greg Klar <gklar@nvidia.com>
2022-02-18 16:09:44 +1300

    Better test for constants

--- tests/test_compile_consts.py
+++ tests/test_compile_consts.py
@@ -1,24 +1,20 @@
 import os
 import sys
 
-from warp.types import Volume
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import unittest
+import test_base
+
 import warp as wp
-import numpy as np
 
 import test_compile_consts_dummy
-# from test_compile_consts_dummy import *
 
 wp.config.mode = "release"
-wp.config.verbose = True
 wp.config.cache_kernels = False
-wp.config.verify_cuda = True
 
 wp.init()
 
-num_points = 10
 
 device = "cuda"
 
@@ -29,16 +25,19 @@ class Foobar:
     TWO = wp.constant(2)
 
 @wp.kernel
-def test(x: wp.array(dtype=int)):
+def test(x: int):
     if Foobar.ONE > 0:
-        x[wp.tid()] = 123 + Foobar.TWO + test_compile_consts_dummy.MINUS_ONE
+        x = 123 + Foobar.TWO + test_compile_consts_dummy.MINUS_ONE
     else:
-        x[wp.tid()] = 456 + LOCAL_ONE
-    
+        x = 456 + LOCAL_ONE
+    expect_eq(x, 124)
 
-x = wp.array([1] * num_points, dtype=int, device=device)
+x = 0
 
-try:
-    wp.launch(kernel=test, dim=num_points, inputs=[x], device=device)
-except Exception as e:
-    print("FAIL: Kernel compilation and launch failed")
+class TestConstants(test_base.TestBase):
+    pass
+
+TestConstants.add_kernel_test(test, dim=1, inputs=[x], devices=["cpu", "cuda"])
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
