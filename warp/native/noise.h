3a01a59947e4408192804f496f70cf8e826820ca
daedalus5 <zach.corse@gmail.com>
2022-02-22 18:28:15 +0000

    quick change to .f

--- warp/native/noise.h
+++ warp/native/noise.h
@@ -54,7 +54,7 @@ inline CUDA_CALLABLE vec3 random_gradient_3d(uint32 seed, int ix, int iy, int iz
     const uint32 p3 = 53471161;
     uint32 idx = ix*p1 ^ iy*p2 ^ iz*p3;
     uint32 state = seed + idx;
-    float theta = acos(1.0 - 2.0 * randf(state));
+    float theta = acos(1.f - 2.f * randf(state));
     float phi = randf(state, 0.f, 2.f*M_PI);
     float x = sin(theta) * cos(phi);
     float y = sin(theta) * sin(phi);
@@ -86,7 +86,7 @@ inline CUDA_CALLABLE vec4 random_gradient_4d(uint32 seed, int ix, int iy, int iz
     uint32 idx = ix*p1 ^ iy*p2 ^ iz*p3 ^ it*p4;
     uint32 state = seed + idx;
     float psi = cdf_root_4d(randf(state));
-    float theta = acos(1.0 - 2.0 * randf(state));
+    float theta = acos(1.f - 2.f * randf(state));
     float phi = randf(state, 0.f, 2.f*M_PI);
     float x = sin(psi) * sin(theta) * cos(phi);
     float y = sin(psi) * sin(theta) * sin(phi);
