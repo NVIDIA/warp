e1fcafbe7965b9529fe3efcfa4c012fae06be4e4
Greg Klar <gklar@nvidia.com>
2022-03-14 14:44:55 +1300

    Fixing auto import issue

--- warp/tape.py
+++ warp/tape.py
@@ -5,7 +5,6 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-from tkinter import E
 import numpy as np
 import warp as wp
 
