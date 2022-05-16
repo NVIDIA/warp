fix noise gradient discontinuities, adding noise_gradient_2D grid cross test

warp/tests/test_noise.py
@@ -13,6 +13,7 @@ import numpy as np
 
 wp.init()
 
+
 @wp.kernel
 def pnoise(
     kernel_seed: int,
@@ -36,6 +37,7 @@ def pnoise(
     g = ((n + 1.0) / 2.0) * 255.0
     pixel_values[tid] = g
 
+
 @wp.kernel
 def curlnoise(
     kernel_seed: int,
@@ -55,6 +57,7 @@ def curlnoise(
 
     noise_coords[tid] = p
     noise_vectors[tid] = v
+
 
 def test_pnoise(test, device):
     # image dim
@@ -100,6 +103,7 @@ def test_pnoise(test, device):
     err = np.max(np.abs(img - img_true))
     tolerance = 1.5e-3
     test.assertTrue(err < tolerance, f"err is {err} which is >= {tolerance}")
+
 
 def test_curlnoise(test, device):
     # image dim
@@ -157,6 +161,92 @@ def test_curlnoise(test, device):
     err = np.max(np.abs(result - result_true))
     test.assertTrue(err < 1e-04)
 
+
+@wp.kernel
+def noise_loss_kernel(
+    kernel_seed: int,
+    query_positions: wp.array(dtype=wp.vec2),
+    noise_values: wp.array(dtype=float),
+    noise_loss: wp.array(dtype=float)):
+
+    tid = wp.tid()
+    state = wp.rand_init(kernel_seed)
+
+    p = query_positions[tid]
+
+    n = wp.noise(state, p)
+    noise_values[tid] = n
+
+    wp.atomic_add(noise_loss, 0, n)
+
+
+@wp.kernel
+def noise_cd(
+    kernel_seed: int,
+    query_positions: wp.array(dtype=wp.vec2),
+    gradients: wp.array(dtype=wp.vec2)):
+    
+    tid = wp.tid()
+    state = wp.rand_init(kernel_seed)
+    p = query_positions[tid]
+
+    eps = 1.e-3
+
+    pl = wp.vec2(p[0] - eps, p[1])
+    pr = wp.vec2(p[0] + eps, p[1])
+    pd = wp.vec2(p[0], p[1] - eps)
+    pu = wp.vec2(p[0], p[1] + eps)
+
+    nl = wp.noise(state, pl)
+    nr = wp.noise(state, pr)
+    nd = wp.noise(state, pd)
+    nu = wp.noise(state, pu)
+
+    gx = (nr - nl) / (2.0*eps)
+    gy = (nu - nd) / (2.0*eps)
+
+    gradients[tid] = wp.vec2(gx, gy)
+
+
+def test_adj_noise(test, device):
+    # grid dim
+    N = 9
+    seed = 42
+
+    tape = wp.Tape()
+
+    positions = np.array(
+        [
+            [-0.1, -0.1], [0.0, -0.1], [0.1, -0.1],
+            [-0.1,  0.0], [0.0,  0.0], [0.1,  0.0],
+            [-0.1,  0.1], [0.0,  0.1], [0.1,  0.1]
+        ]
+    )
+
+    with tape:
+
+        query_positions = wp.array(positions, dtype=wp.vec2, device=device, requires_grad=True)
+        noise_values = wp.zeros(N, dtype=float, device=device)
+        noise_loss = wp.zeros(n=1, dtype=float, device=device)
+
+        wp.launch(kernel=noise_loss_kernel, dim=N, inputs=[seed, query_positions, noise_values, noise_loss], device=device)
+
+    # analytic
+    tape.backward(loss=noise_loss)
+    analytic = tape.gradients[query_positions].numpy().reshape((3,3,2))
+
+    # central difference
+    gradients = wp.zeros(N, dtype=wp.vec2, device=device)
+    wp.launch(kernel=noise_cd, dim=N, inputs=[seed, query_positions, gradients], device=device)
+
+    gradients_host = gradients.numpy().reshape((3,3,2))
+    diff = analytic - gradients_host
+    result = np.sum(diff * diff, axis=2)
+
+    err = np.where(result > 1.e-3, result, 0).sum()
+    test.assertTrue(err < 1.e-8)
+
+
 def register(parent):
 
     devices = wp.get_devices()
@@ -165,7 +255,8 @@ def register(parent):
         pass
 
     add_function_test(TestNoise, "test_pnoise", test_pnoise, devices=devices)
-    # add_function_test(TestNoise, "test_curlnoise", test_curlnoise, devices=devices)
+    add_function_test(TestNoise, "test_curlnoise", test_curlnoise, devices=devices)
+    add_function_test(TestNoise, "test_adj_noise", test_adj_noise, devices=devices)
 
     return TestNoise
 