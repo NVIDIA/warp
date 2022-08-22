aae5f9948d82d16e2326ed361291a4c507be8e60
Zach Corse <zcorse@nvidia.com>
2022-08-22 15:11:59 +0100

    point sample browser to local data dir

--- exts/omni.warp/omni/warp/scripts/extension.py
+++ exts/omni.warp/omni/warp/scripts/extension.py
@@ -5,11 +5,12 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
+import os
 import warp as wp
 import omni.ext
 from .menu import WarpMenu
 from .common import log_info
-
+sample_path = os.path.join(os.path.dirname(__file__), "../../../data/scenes")
 
 class OmniWarpExtension(omni.ext.IExt):
 
@@ -20,7 +21,7 @@ class OmniWarpExtension(omni.ext.IExt):
         self._menu = WarpMenu()
 
         omni.kit.browser.sample.register_sample_folder(
-            "https://omniverse-content-production.s3.us-west-2.amazonaws.com/Assets/Extensions/Samples/Warp",
+            sample_path,
             "Warp"
         )
 
@@ -32,5 +33,5 @@ class OmniWarpExtension(omni.ext.IExt):
         self._menu = None
 
         omni.kit.browser.sample.unregister_sample_folder(
-            "https://omniverse-content-production.s3.us-west-2.amazonaws.com/Assets/Extensions/Samples/Warp"
+            sample_path
         )
