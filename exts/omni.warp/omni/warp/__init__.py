7a3ef476368edb8dd9fd5f23a4ca6d3b5e5302d2
Christopher Crouzet <christopher@crouzet.pm>
2022-11-04 10:16:54 +1300

    Reduce the API exposed through the omni extension

--- exts/omni.warp/omni/warp/__init__.py
+++ exts/omni.warp/omni/warp/__init__.py
@@ -1,7 +1,14 @@
+"""Public API for the omni.warp extension"""
+
+__all__ = [
+    "from_omni_graph",
+]
+
 import ctypes
 
-from .scripts.extension import *
-from .ogn import *
+import warp as wp
+
+from .scripts.extension import OmniWarpExtension
 
 # convert an OmniGraph database attribute to a wp.array
 def from_omni_graph(attr, dtype=float):
