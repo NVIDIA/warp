2b5b3517826998f0ce70c59e3ddb23c86c370121
Eric Heiden <eric-heiden@outlook.com>
2022-11-28 15:57:10 -0800

    Add utilities

---
+++ warp/sim/utils.py
@@ -0,0 +1,103 @@
+import warp as wp
+
+
+@wp.func
+def velocity_at_point(
+    qd: wp.spatial_vector,
+    r: wp.vec3
+):
+    """
+    Returns the velocity of a point relative to the frame with the given spatial velocity.
+    """
+    return wp.cross(wp.spatial_top(qd), r) + wp.spatial_bottom(qd)
+
+
+@wp.func
+def quat_twist(axis: wp.vec3, q: wp.quat):
+    """
+    Returns the twist around an axis.
+    """
+
+    # project imaginary part onto axis
+    a = wp.vec3(q[0], q[1], q[2])
+    a = wp.dot(a, axis)*axis
+
+    return wp.normalize(wp.quat(a[0], a[1], a[2], q[3]))
+
+
+@wp.func
+def quat_decompose(q: wp.quat):
+    """
+    Decompose a quaternion into a sequence of 3 rotations around x,y',z' respectively, i.e.: q = q_z''q_y'q_x.
+    """
+
+    R = wp.mat33(
+        wp.quat_rotate(q, wp.vec3(1.0, 0.0, 0.0)),
+        wp.quat_rotate(q, wp.vec3(0.0, 1.0, 0.0)),
+        wp.quat_rotate(q, wp.vec3(0.0, 0.0, 1.0)))
+
+    # https://www.sedris.org/wg8home/Documents/WG80485.pdf
+    phi = wp.atan2(R[1, 2], R[2, 2])
+    sinp = -R[0, 2]
+    if wp.abs(sinp) >= 1.0:
+        theta = 1.57079632679 * wp.sign(sinp)
+    else:
+        theta = wp.asin(-R[0, 2])
+    psi = wp.atan2(R[0, 1], R[0, 0])
+
+    return -wp.vec3(phi, theta, psi)
+
+
+@wp.func
+def transform_twist(t: wp.transform, x: wp.spatial_vector):
+    # Frank & Park definition 3.20, pg 100
+
+    q = wp.transform_get_rotation(t)
+    p = wp.transform_get_translation(t)
+
+    w = wp.spatial_top(x)
+    v = wp.spatial_bottom(x)
+
+    w = wp.quat_rotate(q, w)
+    v = wp.quat_rotate(q, v) + wp.cross(p, w)
+
+    return wp.spatial_vector(w, v)
+
+
+@wp.func
+def transform_wrench(t: wp.transform, x: wp.spatial_vector):
+
+    q = wp.transform_get_rotation(t)
+    p = wp.transform_get_translation(t)
+
+    w = wp.spatial_top(x)
+    v = wp.spatial_bottom(x)
+
+    v = wp.quat_rotate(q, v)
+    w = wp.quat_rotate(q, w) + wp.cross(p, v)
+
+    return wp.spatial_vector(w, v)
+
+
+@wp.func
+def transform_inertia(t: wp.transform, I: wp.spatial_matrix):
+    """
+    Computes adj_t^-T*I*adj_t^-1 (tensor change of coordinates).
+    (Frank & Park, section 8.2.3, pg 290)
+    """
+
+    t_inv = wp.transform_inverse(t)
+
+    q = wp.transform_get_rotation(t_inv)
+    p = wp.transform_get_translation(t_inv)
+
+    r1 = wp.quat_rotate(q, wp.vec3(1.0, 0.0, 0.0))
+    r2 = wp.quat_rotate(q, wp.vec3(0.0, 1.0, 0.0))
+    r3 = wp.quat_rotate(q, wp.vec3(0.0, 0.0, 1.0))
+
+    R = wp.mat33(r1, r2, r3)
+    S = wp.mul(wp.skew(p), R)
+
+    T = wp.spatial_adjoint(R, S)
+
+    return wp.mul(wp.mul(wp.transpose(T), I), T)
