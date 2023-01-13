2b6eec1cce2a5006c52f735861317dc77de29abd
Zach Corse <zcorse@nvidia.com>
2023-01-13 13:26:20 -0500

    removing rotate_rodrigues

--- warp/native/quat.h
+++ warp/native/quat.h
@@ -188,20 +188,6 @@ inline CUDA_CALLABLE vec3 quat_rotate_inv(const quat& q, const vec3& x)
     return x*(2.0f*q.w*q.w-1.0f) - cross(vec3(&q.x), x)*q.w*2.0f + vec3(&q.x)*dot(vec3(&q.x), x)*2.0f;
 }
 
-inline CUDA_CALLABLE vec3 rotate_rodriguez(const vec3& r, const vec3& x)
-{
-    float angle = length(r);
-    if (angle > kEps || angle < kEps)
-    {
-        vec3 axis = r / angle;
-        return x * cos(angle) + cross(axis, x) * sin(angle) + axis * dot(axis, x) * (1.f - cos(angle));
-    }
-    else
-    {
-        return x;
-    }
-}
-
 inline CUDA_CALLABLE quat quat_slerp(const quat& q0, const quat& q1, float t)
 {
     vec3 axis;
@@ -560,29 +546,6 @@ inline CUDA_CALLABLE void adj_quat_rotate_inv(const quat& q, const vec3& p, quat
         adj_p.y += r.y*(t3+(q.y*q.y)*2.0f-1.0f)+r.x*(t4+q.x*q.y*2.0f)-r.z*(t7-q.y*q.z*2.0f);
         adj_p.z += -r.x*(t5-t6)+r.z*(t3+(q.z*q.z)*2.0f-1.0f)+r.y*(t7+q.y*q.z*2.0f);
     }
-}
-
-inline CUDA_CALLABLE void adj_rotate_rodriguez(const vec3& r, const vec3& x, vec3& adj_r, vec3& adj_x, const vec3& adj_ret)
-{
-    float angle = length(r);
-    float angle_squared = angle * angle;
-
-    vec3 axis = r / angle;
-    mat33 rotation_matrix = quat_to_matrix(quat_from_axis_angle(axis, angle));
-    mat33 inverse_rotation_matrix = transpose(rotation_matrix);
-    mat33 A = mul(mul(rotation_matrix, skew(x)), -1.0);
-
-    if (!angle_squared)
-    {
-        adj_r += mul(transpose(A), adj_ret);
-    }
-    else{
-        float inv_angle_squared = 1.f / angle_squared;
-        mat33 B = mul(add(outer(r,r), mul(sub(inverse_rotation_matrix, diag(vec3(1.f))), skew(r))), inv_angle_squared);
-        adj_r += mul(transpose(mul(A, B)), adj_ret);
-    }
-    
-    // todo: add adj_x
 }
 
 inline CUDA_CALLABLE void adj_quat_slerp(const quat& q0, const quat& q1, float t, quat& adj_q0, quat& adj_q1, float& adj_t, const quat& adj_ret)
