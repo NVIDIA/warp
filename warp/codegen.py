7503dc9cbc9f539980842e94f57910ec845c2d30
Nicolas Capens <ncapens@nvidia.com>
2023-01-30 10:35:15 -0500

    Implement support for break statements in kernels
    Only primal forward execution is supported at this point.

--- warp/codegen.py
+++ warp/codegen.py
@@ -246,6 +246,7 @@ class Adjoint:
 
         # blocks
         adj.blocks = [Block()]
+        adj.loop_blocks = []
 
         # holds current indent level
         adj.prefix = ""
@@ -571,6 +572,7 @@ class Adjoint:
     def begin_for(adj, iter):
 
         cond_block = adj.begin_block()
+        adj.loop_blocks.append(cond_block)
         adj.add_forward(f"for_start_{cond_block.label}:;")
         adj.indent()
 
@@ -589,6 +591,7 @@ class Adjoint:
        
         body_block = adj.end_block()
         cond_block = adj.end_block()
+        adj.loop_blocks.pop()
                 
         ####################
         # forward pass
@@ -635,9 +638,10 @@ class Adjoint:
     # define a while loop
     def begin_while(adj, cond):
 
-        # evaulate condition in it's own block
+        # evaulate condition in its own block
         # so we can control replay
         cond_block = adj.begin_block()
+        adj.loop_blocks.append(cond_block)
         cond_block.body_forward.append(f"while_start_{cond_block.label}:;")
 
         c = adj.eval(cond)
@@ -654,6 +658,7 @@ class Adjoint:
         adj.dedent()     
         body_block = adj.end_block()
         cond_block = adj.end_block()
+        adj.loop_blocks.pop()
 
         ####################
         # forward pass
@@ -1100,6 +1105,9 @@ class Adjoint:
                         adj.symbols[sym] = var1
 
                 adj.end_for(iter)
+
+        elif (isinstance(node, ast.Break)):
+            adj.add_forward(f"goto for_end_{adj.loop_blocks[-1].label};")
 
         elif (isinstance(node, ast.Expr)):
             return adj.eval(node.value)
