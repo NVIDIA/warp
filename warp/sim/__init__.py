1a20baa4c37f045d185747fb3c8554c76428194a
Eric Heiden <eric-heiden@outlook.com>
2023-02-06 19:15:08 +0100

    Expose more model variables

--- warp/sim/__init__.py
+++ warp/sim/__init__.py
@@ -11,10 +11,20 @@ from . model import State, Model, ModelBuilder, Mesh
 from . model import GEO_SPHERE
 from . model import GEO_BOX
 from . model import GEO_CAPSULE
+from . model import GEO_CYLINDER
+from . model import GEO_CONE
 from . model import GEO_MESH
 from . model import GEO_SDF
 from . model import GEO_PLANE
 from . model import GEO_NONE
+
+from . model import JOINT_MODE_LIMIT
+from . model import JOINT_MODE_TARGET_POSITION
+from . model import JOINT_MODE_TARGET_VELOCITY
+
+from . model import JointAxis
+from . model import ShapeContactMaterial
+from . model import GeoProperties
 
 from . model import JOINT_PRISMATIC
 from . model import JOINT_REVOLUTE
@@ -23,6 +33,8 @@ from . model import JOINT_FIXED
 from . model import JOINT_FREE
 from . model import JOINT_COMPOUND
 from . model import JOINT_UNIVERSAL
+from . model import JOINT_DISTANCE
+from . model import JOINT_D6
 
 from . integrator_euler import SemiImplicitIntegrator
 from . integrator_euler import VariationalImplicitIntegrator
@@ -31,7 +43,9 @@ from . integrator_xpbd import XPBDIntegrator
 
 from . collide import collide
 from . articulation import eval_fk, eval_ik
+from . lbs import parse_lbs, update_lbs, update_state_from_transform
 
 from . import_mjcf import parse_mjcf
 from . import_urdf import parse_urdf
 from . import_snu import parse_snu
+from . import_usd import parse_usd
