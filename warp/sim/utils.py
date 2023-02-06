209c1bf6b928f1569517d4ffce112aac8cf85ed8
Eric Heiden <eric-heiden@outlook.com>
2023-02-06 19:19:21 +0100

    More helper functions

--- warp/sim/utils.py
+++ warp/sim/utils.py
@@ -1,5 +1,8 @@
 import warp as wp
 
+PI = wp.constant(3.14159265359)
+PI_2 = wp.constant(1.57079632679)
+
 
 @wp.func
 def velocity_at_point(
@@ -20,9 +23,20 @@ def quat_twist(axis: wp.vec3, q: wp.quat):
 
     # project imaginary part onto axis
     a = wp.vec3(q[0], q[1], q[2])
-    a = wp.dot(a, axis)*axis
-
+    proj = wp.dot(a, axis)
+    a = proj*axis
+    # if proj < 0.0:
+    #     # ensure twist points in same direction as axis
+    #     a = -a
     return wp.normalize(wp.quat(a[0], a[1], a[2], q[3]))
+
+
+@wp.func
+def quat_twist_angle(axis: wp.vec3, q: wp.quat):
+    """
+    Returns the angle of the twist around an axis.
+    """
+    return 2.0 * wp.acos(quat_twist(axis, q)[3])
 
 
 @wp.func
@@ -46,6 +60,73 @@ def quat_decompose(q: wp.quat):
     psi = wp.atan2(R[0, 1], R[0, 0])
 
     return -wp.vec3(phi, theta, psi)
+
+
+@wp.func
+def quat_to_rpy(q: wp.quat):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    x = q[0]
+    y = q[1]
+    z = q[2]
+    w = q[3]
+    t0 = 2.0 * (w * x + y * z)
+    t1 = 1.0 - 2.0 * (x * x + y * y)
+    roll_x = wp.atan2(t0, t1)
+
+    t2 = 2.0 * (w * y - z * x)
+    t2 = wp.clamp(t2, -1.0, 1.0)
+    pitch_y = wp.asin(t2)
+
+    t3 = 2.0 * (w * z + x * y)
+    t4 = 1.0 - 2.0 * (y * y + z * z)
+    yaw_z = wp.atan2(t3, t4)
+
+    return wp.vec3(roll_x, pitch_y, yaw_z)
+
+
+@wp.func
+def quat_to_euler(q: wp.quat, i: int, j: int, k: int) -> wp.vec3:
+    """
+    Convert a quaternion into euler angles
+    i, j, k are the indices in [1,2,3] of the axes to use
+    (i != j, j != k)
+    """
+    # https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0276302
+    not_proper = True
+    if i == k:
+        not_proper = False
+        k = 6 - i - j  # because i + j + k = 1 + 2 + 3 = 6
+    e = float((i-j)*(j-k)*(k-i)) / 2.0  # Levi-Civita symbol
+    a = q[0]
+    b = q[i]
+    c = q[j]
+    d = q[k]*e
+    if not_proper:
+        a -= q[j]
+        b += q[k]*e
+        c += q[0]
+        d -= q[i]
+    t2 = wp.acos(2.0*(a*a + b*b)/(a*a + b*b + c*c + d*d) - 1.0)
+    tp = wp.atan2(b, a)
+    tm = wp.atan2(d, c)
+    t1 = 0.0
+    t3 = 0.0
+    if wp.abs(t2) < 1e-6:
+        t3 = 2.0*tp - t1
+    elif wp.abs(t2-PI_2) < 1e-6:
+        t3 = 2.0*tm + t1
+    else:
+        t1 = tp - tm
+        t3 = tp + tm
+    if not_proper:
+        t2 -= PI_2
+        t3 *= e
+    return wp.vec3(t1, t2, t3)
 
 
 @wp.func
