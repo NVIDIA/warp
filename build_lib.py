5f2b69a3433b073515d5b59f57a39f533c0f0940
Nicolas Capens <ncapens@nvidia.com>
2023-03-14 15:39:27 -0400

    Print log info about cloning LLVM

--- build_lib.py
+++ build_lib.py
@@ -78,17 +78,22 @@ if args.build_llvm:
     import subprocess
     from git import Repo
 
-    llvm_project_path = os.path.join(base_path, "external/llvm-project")
+    llvm_project_dir = "external/llvm-project"
+    llvm_project_path = os.path.join(base_path, llvm_project_dir)
+    repo_url = "https://github.com/llvm/llvm-project.git"
+
     if not os.path.exists(llvm_project_path):
+        print(f"Cloning LLVM project...")
         shallow_clone = True  # https://github.blog/2020-12-21-get-up-to-speed-with-partial-clone-and-shallow-clone/
         if shallow_clone:
-            repo = Repo.clone_from("https://github.com/llvm/llvm-project.git", to_path=llvm_project_path, single_branch=True, branch="llvmorg-15.0.7", depth=1)
+            repo = Repo.clone_from(repo_url, to_path=llvm_project_path, single_branch=True, branch="llvmorg-15.0.7", depth=1)
         else:
-            repo = Repo.clone_from("https://github.com/llvm/llvm-project.git", llvm_project_path)
+            repo = Repo.clone_from(repo_url, to_path=llvm_project_path)
             repo.git.checkout("tags/llvmorg-15.0.7", "-b", "llvm-15.0.7")
     else:
+        print(f"Found existing {llvm_project_dir} directory")
         repo = Repo(llvm_project_path)
-    
+
     # CMake supports Debug, Release, RelWithDebInfo, and MinSizeRel builds
     if warp.config.mode == "release":
         cmake_build_type = "MinSizeRel"  # prefer smaller size over aggressive speed
