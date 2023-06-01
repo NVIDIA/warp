4ba34d9f7b33b0c112481926b6ea65dd5ee67f89
Nicolas Capens <ncapens@nvidia.com>
2023-06-01 16:16:54 -0400

    Set the distribution package platform based on the warp/bin libraries
    When creating a distribution package with `python3 -m build --wheel` it
    previously assumed the libraries in warp/bin to just be data. That
    resulted in packages named `warp_lang-{version}-py3-none-any.whl`, which
    works cross-platform as long as libraries for all platforms are
    included.
    
    Recently this became impractical due to exceeding the PyPi 100 MB limit
    when including the standalone Clang/LLVM compiler for Windows, Linux,
    and macOS.
    
    This change makes it easier to create platform-specific packages which
    adhere to the compatibility tags convention laid out in
    https://packaging.python.org/en/latest/specifications/platform-compatibility-tags
    by inspecting the file extension of the libraries in the `warp/bin`
    directory, creating a package with the correct platform tag, independent
    of the system on which the package is built.

--- setup.py
+++ setup.py
@@ -1,4 +1,39 @@
 import setuptools
+import os
+from wheel.bdist_wheel import bdist_wheel
+
+
+def get_warp_platform():
+    for filename in os.listdir("warp/bin"):
+        if os.path.splitext(filename)[1] == ".dll":
+            return "win_amd64"
+        if os.path.splitext(filename)[1] == ".so":
+            return "manylinux2014_x86_64"
+        if os.path.splitext(filename)[1] == ".dylib":
+            return "macosx_10_13_x86_64"
+
+    raise Exception("No libraries found in warp/bin")
+
+
+# Distributions are identified as non-pure (i.e. containing non-Python code, or binaries) if the
+# setuptools.setup() `ext_modules` parameter is not emtpy, but this assumes building extension
+# modules from source through the Python build. This class provides an override for prebuilt binaries:
+class BinaryDistribution(setuptools.Distribution):
+    def has_ext_modules(self):
+        return True
+
+
+# Binary wheel distribution builds assume that the platform you're building on will be the platform
+# of the package. This factory function provides a class which overrides the platform tag.
+# https://packaging.python.org/en/latest/specifications/platform-compatibility-tags
+def bdist_factory(platform_tag: str):
+    class warp_bdist(bdist_wheel):
+        def get_tag(self, *args, **kws):
+            # The tag format is {python tag}-{abi tag}-{platform tag}.
+            return "py3", "none", platform_tag
+
+    return warp_bdist
+
 
 setuptools.setup(
     name="warp-lang",
@@ -34,6 +69,8 @@ setuptools.setup(
         "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
     ],
+    distclass=BinaryDistribution,
+    cmdclass={"bdist_wheel": bdist_factory(get_warp_platform())},
     install_requires=["numpy"],
     python_requires=">=3.7",
 )
