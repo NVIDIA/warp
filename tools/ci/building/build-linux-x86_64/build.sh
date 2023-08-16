37c26e0fe802d592b7a43e998d618a1e266f80c7
Eric Shi <ershi@nvidia.com>
2023-08-15 20:07:04 -0700

    x86 (Windows+Linux) build scripts accept --debug

--- tools/ci/building/build-linux-x86_64/build.sh
+++ tools/ci/building/build-linux-x86_64/build.sh
@@ -2,6 +2,7 @@
 set -e
 
 USE_LINBUILD=1
+BUILD_MODE="release"
 
 # scan command line for options
 for arg; do
@@ -10,9 +11,15 @@ for arg; do
     --no-docker)
         USE_LINBUILD=0
         ;;
+    --debug)
+        BUILD_MODE="debug"
+        ;;
     esac
+
     # keep all options (including --no-docker) to pass to repo.sh
-    set -- "$@" "$arg"
+    if [[ $arg != "--debug" ]]; then
+        set -- "$@" "$arg"
+    fi 
 done
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
@@ -25,6 +32,7 @@ LINBUILD="$SCRIPT_DIR/../../../../_build/host-deps/linbuild/linbuild.sh"
 CUDA="$SCRIPT_DIR/../../../../_build/target-deps/cuda"
 
 # pip deps
+$PYTHON -m pip install --upgrade pip
 $PYTHON -m pip install numpy
 $PYTHON -m pip install gitpython
 $PYTHON -m pip install cmake
@@ -35,9 +43,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
     if [ ${USE_LINBUILD} -ne 0 ]; then
         # build with docker for increased compatibility
-        $LINBUILD -- $PYTHON "$SCRIPT_DIR/../../../../build_lib.py" --cuda_path=$CUDA
+        $LINBUILD -- $PYTHON "$SCRIPT_DIR/../../../../build_lib.py" --cuda_path=$CUDA --mode=$BUILD_MODE
     else
         # build without docker
-        $PYTHON "$SCRIPT_DIR/../../../../build_lib.py" --cuda_path=$CUDA
+        $PYTHON "$SCRIPT_DIR/../../../../build_lib.py" --cuda_path=$CUDA --mode=$BUILD_MODE
     fi
 fi
