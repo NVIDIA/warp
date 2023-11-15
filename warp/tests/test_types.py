a69872f79cd269eaac08eab358d16b3f7c565f5c
Christopher Crouzet <christopher@crouzet.pm>
2023-11-15 15:46:01 +1300

    Increase coverage of vector and matrix types

--- warp/tests/test_types.py
+++ warp/tests/test_types.py
@@ -105,6 +105,50 @@ def test_vector(test, device, dtype):
     test.assertSequenceEqual(v / dtype(2), make_vec(1, 2, 3))
     test.assertSequenceEqual(dtype(12) / v, make_vec(6, 3, 2))
 
+    test.assertTrue(v != vec3_cls(1, 2, 3))
+    test.assertEqual(str(v), "[{}]".format(", ".join(str(x) for x in v)))
+
+    # Check added purely for coverage reasons but is this really a desired
+    # behaviour? Not allowing to define new attributes using systems like
+    # `__slots__` could help improving memory usage.
+    v.foo = 123
+    test.assertEqual(v.foo, 123)
+
+
+def test_vector_error_invalid_arg_count(test, device):
+    with test.assertRaisesRegex(
+        ValueError,
+        r"Invalid number of arguments in vector constructor, expected 3 elements, got 2$",
+    ):
+        wp.vec3(1, 2)
+
+
+def test_vector_error_invalid_ptr(test, device):
+    with test.assertRaisesRegex(
+        RuntimeError,
+        r"NULL pointer exception",
+    ):
+        wp.vec3.from_ptr(0)
+
+
+def test_vector_error_invalid_get_item_key(test, device):
+    v = wp.vec3(1, 2, 3)
+
+    with test.assertRaisesRegex(
+        KeyError,
+        r"Invalid key None, expected int or slice",
+    ):
+        v[None]
+
+
+def test_vector_error_invalid_set_item_key(test, device):
+    v = wp.vec3(1, 2, 3)
+    with test.assertRaisesRegex(
+        KeyError,
+        r"Invalid key None, expected int or slice",
+    ):
+        v[None] = 0
+
 
 def test_matrix(test, device, dtype):
     def make_scalar(x):
@@ -130,6 +174,7 @@ def test_matrix(test, device, dtype):
 
     mat22_cls = wp.mat((2, 2), dtype)
     mat33_cls = wp.mat((3, 3), dtype)
+    vec2_cls = wp.vec(2, dtype)
 
     m = mat33_cls(((1, 2, 3), (4, 5, 6), (7, 8, 9)))
     test.assertEqual(m[0][0], make_scalar(1))
@@ -231,6 +276,94 @@ def test_matrix(test, device, dtype):
     test.assertSequenceEqual(m / dtype(2), make_mat((1, 2), (3, 4)))
     test.assertSequenceEqual(dtype(24) / m, make_mat((12, 6), (4, 3)))
 
+    test.assertSequenceEqual(m * vec2_cls(1, 2), make_vec(10, 22))
+    test.assertSequenceEqual(m @ vec2_cls(1, 2), make_vec(10, 22))
+    test.assertSequenceEqual(vec2_cls(1, 2) * m, make_vec(14, 20))
+    test.assertSequenceEqual(vec2_cls(1, 2) @ m, make_vec(14, 20))
+
+    test.assertTrue(m != mat22_cls(1, 2, 3, 4))
+    test.assertEqual(
+        str(m), "[{}]".format(",\n ".join("[{}]".format(", ".join(str(y) for y in m[x])) for x in range(m._shape_[0])))
+    )
+
+    # Check added purely for coverage reasons but is this really a desired
+    # behaviour? Not allowing to define new attributes using systems like
+    # `__slots__` could help improving memory usage.
+    m.foo = 123
+    test.assertEqual(m.foo, 123)
+
+
+def test_matrix_error_invalid_arg_count(test, device):
+    with test.assertRaisesRegex(
+        ValueError,
+        r"Invalid number of arguments in matrix constructor, expected 4 elements, got 3$",
+    ):
+        wp.mat22(1, 2, 3)
+
+
+def test_matrix_error_invalid_row_count(test, device):
+    with test.assertRaisesRegex(
+        TypeError,
+        r"Invalid argument in matrix constructor, expected row of length 2, got \(1, 2, 3\)$",
+    ):
+        wp.mat22((1, 2, 3), (3, 4, 5))
+
+
+def test_matrix_error_invalid_ptr(test, device):
+    with test.assertRaisesRegex(
+        RuntimeError,
+        r"NULL pointer exception",
+    ):
+        wp.mat22.from_ptr(0)
+
+
+def test_matrix_error_invalid_set_row_index(test, device):
+    m = wp.mat22(1, 2, 3, 4)
+    with test.assertRaisesRegex(
+        IndexError,
+        r"Invalid row index$",
+    ):
+        m.set_row(2, (0, 0))
+
+
+def test_matrix_error_invalid_get_item_key(test, device):
+    m = wp.mat22(1, 2, 3, 4)
+
+    with test.assertRaisesRegex(
+        KeyError,
+        r"Invalid key None, expected int or pair of ints",
+    ):
+        m[None]
+
+
+def test_matrix_error_invalid_get_item_key_length(test, device):
+    m = wp.mat22(1, 2, 3, 4)
+
+    with test.assertRaisesRegex(
+        KeyError,
+        r"Invalid key, expected one or two indices, got 3",
+    ):
+        m[0, 1, 2]
+
+
+def test_matrix_error_invalid_set_item_key(test, device):
+    m = wp.mat22(1, 2, 3, 4)
+    with test.assertRaisesRegex(
+        KeyError,
+        r"Invalid key None, expected int or pair of ints",
+    ):
+        m[None] = 0
+
+
+def test_matrix_error_invalid_set_item_key_length(test, device):
+    m = wp.mat22(1, 2, 3, 4)
+
+    with test.assertRaisesRegex(
+        KeyError,
+        r"Invalid key, expected one or two indices, got 3",
+    ):
+        m[0, 1, 2] = (0, 0)
+
 
 def register(parent):
     devices = get_test_devices()
@@ -244,8 +377,22 @@ def register(parent):
     for dtype in tuple(wp.types.scalar_types) + (int, float):
         add_function_test(TestTypes, f"test_vector_{dtype.__name__}", test_vector, devices=None, dtype=dtype)
 
+    add_function_test(TestTypes, "test_vector_error_invalid_arg_count", test_vector_error_invalid_arg_count)
+    add_function_test(TestTypes, "test_vector_error_invalid_ptr", test_vector_error_invalid_ptr)
+    add_function_test(TestTypes, "test_vector_error_invalid_get_item_key", test_vector_error_invalid_get_item_key)
+    add_function_test(TestTypes, "test_vector_error_invalid_set_item_key", test_vector_error_invalid_set_item_key)
+
     for dtype in tuple(wp.types.float_types) + (float,):
         add_function_test(TestTypes, f"test_matrix_{dtype.__name__}", test_matrix, devices=None, dtype=dtype)
+
+    add_function_test(TestTypes, "test_matrix_error_invalid_arg_count", test_matrix_error_invalid_arg_count)
+    add_function_test(TestTypes, "test_matrix_error_invalid_row_count", test_matrix_error_invalid_row_count)
+    add_function_test(TestTypes, "test_matrix_error_invalid_ptr", test_matrix_error_invalid_ptr)
+    add_function_test(TestTypes, "test_matrix_error_invalid_set_row_index", test_matrix_error_invalid_set_row_index)
+    add_function_test(TestTypes, "test_matrix_error_invalid_get_item_key", test_matrix_error_invalid_get_item_key)
+    add_function_test(TestTypes, "test_matrix_error_invalid_get_item_key_length", test_matrix_error_invalid_get_item_key_length)
+    add_function_test(TestTypes, "test_matrix_error_invalid_set_item_key", test_matrix_error_invalid_set_item_key)
+    add_function_test(TestTypes, "test_matrix_error_invalid_set_item_key_length", test_matrix_error_invalid_set_item_key_length)
 
     return TestTypes
 
