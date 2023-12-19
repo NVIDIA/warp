40e37410bfdc7b532debc7cec74e68eb35a2491d
Christopher Crouzet <christopher@crouzet.pm>
2023-12-19 16:15:09 +1300

    Increase indentation only when printing

--- warp/utils.py
+++ warp/utils.py
@@ -629,7 +629,8 @@ class ScopedTimer:
                 return
 
             self.start = timeit.default_timer()
-            ScopedTimer.indent += 1
+            if self.print:
+                ScopedTimer.indent += 1
 
             if self.detailed:
                 self.cp = cProfile.Profile()
