a2119ccafcd74898583126d65fde8413149de4b5
Nicolas Capens <ncapens@nvidia.com>
2024-01-24 16:03:37 -0500

    Don't strip debug symbols for debug builds

--- warp/build_dll.py
+++ warp/build_dll.py
@@ -356,13 +356,14 @@ def build_dll_for_arch(args, dll_path, cpp_paths, cu_path, libs, arch, mode=None
             run_cmd(link_cmd)
 
             # Strip symbols to reduce the binary size
-            if sys.platform == "darwin":
-                run_cmd(f"strip -x {dll_path}")  # Strip all local symbols
-            else:  # Linux
-                # Strip all symbols except for those needed to support debugging JIT-compiled code
-                run_cmd(
-                    f"strip --strip-all --keep-symbol=__jit_debug_register_code --keep-symbol=__jit_debug_descriptor {dll_path}"
-                )
+            if mode == "release":
+                if sys.platform == "darwin":
+                    run_cmd(f"strip -x {dll_path}")  # Strip all local symbols
+                else:  # Linux
+                    # Strip all symbols except for those needed to support debugging JIT-compiled code
+                    run_cmd(
+                        f"strip --strip-all --keep-symbol=__jit_debug_register_code --keep-symbol=__jit_debug_descriptor {dll_path}"
+                    )
 
 
 def build_dll(args, dll_path, cpp_paths, cu_path, libs=None):
