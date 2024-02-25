92789f4c078bf4ad3767b37f179e1816e9827a6e
Christopher Crouzet <christopher@crouzet.pm>
2024-02-26 09:31:09 +1300

    Update version strings

--- warp/config.py
+++ warp/config.py
@@ -5,7 +5,7 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-version = "0.13.1"
+version = "0.14.0"
 
 verify_fp = False  # verify inputs and outputs are finite after each launch
 verify_cuda = False  # if true will check CUDA errors after each kernel launch / memory operation
