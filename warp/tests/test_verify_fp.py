aacb993b121a9f01f3d1853bb99f48518cf615f6
Nicolas Capens <ncapens@nvidia.com>
2024-03-04 14:31:19 -0500

    Add a NaN test for wp.config.verify_fp
    Bug omniverse/warp#10

--- warp/tests/test_verify_fp.py
+++ warp/tests/test_verify_fp.py
@@ -50,6 +50,34 @@ def test_finite(test, device):
             raise AssertionError(f"Unexpected result, got: {f} expected: {expected}")
 
 
+@wp.kernel
+def nan_kernel(foos: wp.array(dtype=TestStruct)):
+    i = wp.tid()
+    foos[i].field /= wp.float32(0.0)  # Division by zero produces Not-a-Number (NaN)
+
+
+def test_nan(test, device):
+    foos = wp.zeros((10,), dtype=TestStruct, device=device)
+
+    capture = StdOutCapture()
+    capture.begin()
+
+    wp.launch(
+        kernel=nan_kernel,
+        dim=(10,),
+        inputs=[foos],
+        device=device,
+    )
+    wp.synchronize()
+
+    output = capture.end()
+
+    # Check that the output contains warnings about "nan" being produced.
+    # Older Windows C runtimes have a bug where stdout sometimes does not get properly flushed.
+    if output != "" or device.is_cuda or sys.platform != "win32":
+        test.assertRegex(output, r"nan")
+
+
 devices = get_test_devices()
 
 
@@ -58,6 +86,7 @@ class TestVerifyFP(unittest.TestCase):
 
 
 add_function_test(TestVerifyFP, "test_finite", test_finite, devices=devices)
+add_function_test(TestVerifyFP, "test_nan", test_nan, devices=devices, check_output=False)
 
 
 if __name__ == "__main__":
