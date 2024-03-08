2263810841817897723bdeb674ff3fad1a34521f
Eric Shi <ershi@nvidia.com>
2024-03-08 12:08:11 -0800

    Second attempt at fixing build_docs.py on Windows

--- docs/conf.py
+++ docs/conf.py
@@ -36,11 +36,10 @@ extensions = [
     "sphinx.ext.napoleon",  # Convert docstrings to reStructuredText
     "sphinx.ext.intersphinx",
     "sphinx.ext.autosummary",
-    "sphinx.ext.todo",
     "sphinx.ext.extlinks",  # Markup to shorten external links
     "sphinx.ext.githubpages",
     # Third-party extensions:
-    "sphinx_copybutton"
+    "sphinx_copybutton",
     # 'sphinx_tabs.tabs',
     #    'autodocsumm'
 ]
@@ -54,9 +53,6 @@ autodoc_member_order = "bysource"
 
 # autodoc_typehints_format
 # add_module_names = False
-
-todo_include_todos = True
-todo_emit_warnings = True
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
