86620ef700519b845186726ef1cc5d98efb89c9c
Christopher Crouzet <christopher@crouzet.pm>
2024-06-12 08:04:33 +1200

    Add tests to detect uninitialized runtime

--- warp/context.py
+++ warp/context.py
@@ -347,7 +347,7 @@ class Function:
 def call_builtin(func: Function, *params) -> Tuple[bool, Any]:
     uses_non_warp_array_type = False
 
-    warp.context.init()
+    init()
 
     # Retrieve the built-in function from Warp's dll.
     c_func = getattr(warp.context.runtime.core, func.mangled_name)
---
+++ warp/tests/test_implicit_init.py
@@ -0,0 +1,354 @@
+# Copyright (c) 2024 NVIDIA CORPORATION.  All rights reserved.
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+import unittest
+
+import warp as wp
+from warp.tests.unittest_utils import *
+
+#   Array Initialization
+# ------------------------------------------------------------------------------
+
+
+def test_array_from_data(test, device):
+    wp.array((1.0, 2.0, 3.0), dtype=float)
+
+
+class TestImplicitInitArrayFromData(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitArrayFromData,
+    "test_array_from_data",
+    test_array_from_data,
+    check_output=False,
+)
+
+
+def test_array_from_ptr(test, device):
+    wp.array(ptr=0, shape=(123,), dtype=float)
+
+
+class TestImplicitInitArrayFromPtr(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitArrayFromPtr,
+    "test_array_from_ptr",
+    test_array_from_ptr,
+    check_output=False,
+)
+
+
+#   Builtin Call
+# ------------------------------------------------------------------------------
+
+
+def test_builtin_call(test, device):
+    wp.sin(1.23)
+
+
+class TestImplicitInitBuiltinCall(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitBuiltinCall,
+    "test_builtin_call",
+    test_builtin_call,
+    check_output=False,
+)
+
+
+#   Devices
+# ------------------------------------------------------------------------------
+
+
+def test_get_cuda_device_count(test, device):
+    wp.get_cuda_device_count()
+
+
+class TestImplicitInitGetCudaDeviceCount(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitGetCudaDeviceCount,
+    "test_get_cuda_device_count",
+    test_get_cuda_device_count,
+    check_output=False,
+)
+
+
+def test_get_cuda_devices(test, device):
+    wp.get_cuda_devices()
+
+
+class TestImplicitInitGetCudaDevices(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitGetCudaDevices,
+    "test_get_cuda_devices",
+    test_get_cuda_devices,
+    check_output=False,
+)
+
+
+def test_get_device(test, device):
+    wp.get_device("cpu")
+
+
+class TestImplicitInitGetDevice(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitGetDevice,
+    "test_get_device",
+    test_get_device,
+    check_output=False,
+)
+
+
+def test_get_devices(test, device):
+    wp.get_devices()
+
+
+class TestImplicitInitGetDevices(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitGetDevices,
+    "test_get_devices",
+    test_get_devices,
+    check_output=False,
+)
+
+
+def test_get_preferred_device(test, device):
+    wp.get_preferred_device()
+
+
+class TestImplicitInitGetPreferredDevice(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitGetPreferredDevice,
+    "test_get_preferred_device",
+    test_get_preferred_device,
+    check_output=False,
+)
+
+
+def test_is_cpu_available(test, device):
+    wp.is_cpu_available()
+
+
+class TestImplicitInitIsCpuAvailable(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsCpuAvailable,
+    "test_is_cpu_available",
+    test_is_cpu_available,
+    check_output=False,
+)
+
+
+def test_is_cuda_available(test, device):
+    wp.is_cuda_available()
+
+
+class TestImplicitInitIsCudaAvailable(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsCudaAvailable,
+    "test_is_cuda_available",
+    test_is_cuda_available,
+    check_output=False,
+)
+
+
+def test_is_device_available(test, device):
+    wp.is_device_available("cpu")
+
+
+class TestImplicitInitIsDeviceAvailable(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsDeviceAvailable,
+    "test_is_device_available",
+    test_is_device_available,
+    check_output=False,
+)
+
+
+def test_set_device(test, device):
+    wp.set_device("cpu")
+
+
+class TestImplicitInitSetDevice(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitSetDevice,
+    "test_set_device",
+    test_set_device,
+    check_output=False,
+)
+
+
+#   Launch
+# ------------------------------------------------------------------------------
+
+
+@wp.kernel
+def launch_kernel():
+    pass
+
+
+def test_launch(test, device):
+    wp.launch(launch_kernel, dim=1)
+
+
+class TestImplicitInitLaunch(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitLaunch,
+    "test_launch",
+    test_launch,
+    check_output=False,
+)
+
+
+#   Mempool
+# ------------------------------------------------------------------------------
+
+
+def test_is_mempool_enabled(test, device):
+    wp.is_mempool_enabled("cpu")
+
+
+class TestImplicitInitIsMempoolEnabled(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsMempoolEnabled,
+    "test_is_mempool_enabled",
+    test_is_mempool_enabled,
+    check_output=False,
+)
+
+
+def test_is_mempool_supported(test, device):
+    wp.is_mempool_supported("cpu")
+
+
+class TestImplicitInitIsMempoolSupported(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsMempoolSupported,
+    "test_is_mempool_supported",
+    test_is_mempool_supported,
+    check_output=False,
+)
+
+
+#   Mempool Access
+# ------------------------------------------------------------------------------
+
+
+def test_is_mempool_access_enabled(test, device):
+    wp.is_mempool_access_enabled("cpu", "cpu")
+
+
+class TestImplicitInitIsMempoolAccessEnabled(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsMempoolAccessEnabled,
+    "test_is_mempool_access_enabled",
+    test_is_mempool_access_enabled,
+    check_output=False,
+)
+
+
+def test_is_mempool_access_supported(test, device):
+    wp.is_mempool_access_supported("cpu", "cpu")
+
+
+class TestImplicitInitIsMempoolAccessSupported(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsMempoolAccessSupported,
+    "test_is_mempool_access_supported",
+    test_is_mempool_access_supported,
+    check_output=False,
+)
+
+
+#   Peer Access
+# ------------------------------------------------------------------------------
+
+
+def test_is_peer_access_enabled(test, device):
+    wp.is_peer_access_enabled("cpu", "cpu")
+
+
+class TestImplicitInitIsPeerAccessEnabled(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsPeerAccessEnabled,
+    "test_is_peer_access_enabled",
+    test_is_peer_access_enabled,
+    check_output=False,
+)
+
+
+def test_is_peer_access_supported(test, device):
+    wp.is_peer_access_supported("cpu", "cpu")
+
+
+class TestImplicitInitIsPeerAccessSupported(unittest.TestCase):
+    pass
+
+
+add_function_test(
+    TestImplicitInitIsPeerAccessSupported,
+    "test_is_peer_access_supported",
+    test_is_peer_access_supported,
+    check_output=False,
+)
+
+
+if __name__ == "__main__":
+    # Do not clear the kernel cache or call anything that would initialize Warp
+    # since these tests are specifically aiming to catch issues where Warp isn't
+    # correctly initialized upon calling certain public APIs.
+    unittest.main(verbosity=2, failfast=True)
