f5ee1620fd5436e6d01c16ea31d89e9e0a61d793
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-06-17 18:05:06 +0900

    chore: update ell_gemm.h
    Colums -> Columns

--- warp/native/cutlass/include/cutlass/gemm/device/ell_gemm.h
+++ warp/native/cutlass/include/cutlass/gemm/device/ell_gemm.h
@@ -99,7 +99,7 @@ namespace device {
 
     Description of parameters and tensors used to represent the Blocked-Ellpack (ELL) format:
       a_rows              - Rows in the sparse matrix.
-      a_cols              - Colums in the sparse matrix.
+      a_cols              - Columns in the sparse matrix.
       BlockedEllA         - Packed matrix (ellValue matrix) that stores non-zero values in 
                             consecutive blocks, whose size is (a_rows * a_ell_num_columns)
       ell_idx             - Blocked-ELL Column indices (ellColInd) matrix, whose size is
