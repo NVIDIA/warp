1bfb95e599064843d3306c13f9ed207ca471fa21
Lukasz Wawrzyniak <lwawrzyniak@nvidia.com>
2024-06-19 15:49:51 -0400

    Bump to v1.2.2

--- warp/config.py
+++ warp/config.py
@@ -7,7 +7,7 @@
 
 from typing import Optional
 
-version: str = "1.2.1"
+version: str = "1.2.2"
 
 verify_fp: bool = False  # verify inputs and outputs are finite after each launch
 verify_cuda: bool = False  # if true will check CUDA errors after each kernel launch / memory operation
