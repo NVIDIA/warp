bae12df4e8fa4f466c8d6f3562a9305e2419ae32
Eric Shi <ershi@nvidia.com>
2024-08-30 15:05:57 -0700

    Update Packman to 7.24.1

--- tools/packman/bootstrap/install_package.py
+++ tools/packman/bootstrap/install_package.py
@@ -142,7 +142,7 @@ def generate_sha256_for_file(file_path: Union[str, os.PathLike]) -> str:
 
 
 def install_common_module(package_path, install_path):
-    COMMON_SHA256 = "0a2064434cca0170411c86f23349f9618556dc380d3589a2361db38ffeea9cac"
+    COMMON_SHA256 = "ce46783c5a938082514e796a014f8cca5870d6466c6a7147c35e230911dff143"
     package_sha256 = generate_sha256_for_file(package_path)
     if package_sha256 != COMMON_SHA256:
         raise RuntimeError(
