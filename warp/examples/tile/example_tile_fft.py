8fe55f551ac211a0b9ef30ec2f80ad03b7d0d14a
Eric Shi <ershi@nvidia.com>
2024-10-30 15:26:17 -0700

    Rename example, add copyright headers

--- warp/examples/tile/example_tile_fft.py
+++ warp/examples/tile/example_tile_fft.py
@@ -1,11 +1,22 @@
+# Copyright (c) 2024 NVIDIA CORPORATION.  All rights reserved.
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+###########################################################################
+# Example Tile FFT
+#
+# Shows how to write a simple FFT kernel using Warp tile primitives.
+#
+###########################################################################
+
 import numpy as np
 
 import warp as wp
 
-wp.init()
 wp.set_module_options({"enable_backward": False})
-wp.set_device("cuda:0")
-wp.build.clear_kernel_cache()
 
 BLOCK_DIM = 8
 TILE_M = 1
@@ -21,13 +32,16 @@ def fft_tiled(x: wp.array2d(dtype=wp.vec2d), y: wp.array2d(dtype=wp.vec2d)):
     wp.tile_store(y, i, j, a)
 
 
-x_h = np.ones((TILE_M, TILE_N, 2), dtype=np.float64)
-x_h[:, :, 1] = 0
-y_h = 3 * np.ones((TILE_M, TILE_N, 2), dtype=np.float64)
-x_wp = wp.array2d(x_h, dtype=wp.vec2d)
-y_wp = wp.array2d(y_h, dtype=wp.vec2d)
+if __name__ == "__main__":
+    wp.set_device("cuda:0")
 
-wp.launch(fft_tiled, dim=[1, 1, BLOCK_DIM], inputs=[x_wp, y_wp], block_dim=BLOCK_DIM)
+    x_h = np.ones((TILE_M, TILE_N, 2), dtype=np.float64)
+    x_h[:, :, 1] = 0
+    y_h = 3 * np.ones((TILE_M, TILE_N, 2), dtype=np.float64)
+    x_wp = wp.array2d(x_h, dtype=wp.vec2d)
+    y_wp = wp.array2d(y_h, dtype=wp.vec2d)
 
-print("inputs:\n", x_wp)  # [1+0i, 1+0i, 1+0i, ...]
-print("output:\n", y_wp)  # [32+0i, 0, 0, ...]
+    wp.launch_tiled(fft_tiled, dim=[1, 1], inputs=[x_wp], outputs=[y_wp], block_dim=BLOCK_DIM)
+
+    print("Inputs:\n", x_wp)  # [1+0i, 1+0i, 1+0i, ...]
+    print("Output:\n", y_wp)  # [32+0i, 0, 0, ...]
