a8dcc11491bfd05a1f614bada8a41f30c24a8316
Eric Shi <ershi@nvidia.com>
2025-01-03 10:13:04 -0800

    Add doctest run to build_docs.py

--- build_docs.py
+++ build_docs.py
@@ -5,11 +5,20 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
+import argparse
 import os
 import shutil
 import subprocess
 
 from warp.context import export_functions_rst, export_stubs
+
+parser = argparse.ArgumentParser(description="Warp Sphinx Documentation Builder")
+# Note argparse.BooleanOptionalAction can be used here when Python 3.9+ becomes the minimum supported version
+parser.add_argument("--doctest", action="store_true", help="Also run doctest on code snippets, default enabled")
+parser.add_argument("--no_doctest", dest="doctest", action="store_false")
+parser.set_defaults(doctest=True)
+
+args = parser.parse_args()
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -33,5 +42,17 @@ if os.path.exists(output_dir):
 command = ["sphinx-build", "-W", "-b", "html", source_dir, output_dir]
 
 subprocess.run(command, check=True)
+
+if args.doctest:
+    print("Running doctest... (skip with --no_doctest)")
+
+    output_dir = os.path.join(base_path, "docs", "_build", "doctest")
+
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+
+    command = ["sphinx-build", "-W", "-b", "doctest", source_dir, output_dir]
+
+    subprocess.run(command, check=True)
 
 print("Finished")
