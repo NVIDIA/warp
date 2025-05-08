c4f0b16a3c0f7136d9045741c5016c649f2534b6
Yuyang Li <y.li@stu.pku.edu.cn>
2025-05-08 11:10:42 +0800

    fix: resolve issues concerning on-the-fly cache generation in the PR review
    Signed-off-by: Yuyang Li <y.li@stu.pku.edu.cn>

--- warp/tests/misc/add_kernel.py
+++ warp/tests/misc/add_kernel.py
@@ -3,6 +3,5 @@ import warp as wp
 
 @wp.kernel
 def add_kernel(a: wp.array(dtype=wp.int32), b: wp.array(dtype=wp.int32), res: wp.array(dtype=wp.int32)):
-    pass
-    # i = wp.tid()
-    # res[i] = a[i] + b[i]
+    i = wp.tid()
+    res[i] = a[i] + b[i]
