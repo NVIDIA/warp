458e96e1143d254ade6d2d82eb1ee0d14e1df4f6
Christopher Crouzet <christopher@crouzet.pm>
2026-02-02 07:33:50 +1300

    Categorize the utils module

--- docs/api_reference/warp_utils.rst
+++ docs/api_reference/warp_utils.rst
@@ -6,22 +6,46 @@ warp.utils
 
 .. currentmodule:: warp.utils
 
-API
----
+Array Operations
+----------------
+
+.. autosummary::
+   :nosignatures:
+   :toctree: _generated
+
+   array_cast
+   array_inner
+   array_scan
+   array_sum
+
+Sorting
+-------
+
+.. autosummary::
+   :nosignatures:
+   :toctree: _generated
+
+   radix_sort_pairs
+   runlength_encode
+   segmented_sort_pairs
+
+Graph Coloring
+--------------
 
 .. autosummary::
    :nosignatures:
    :toctree: _generated
 
    GraphColoringAlgorithm
-   array_cast
-   array_inner
-   array_scan
-   array_sum
-   create_warp_function
    graph_coloring_assign
    graph_coloring_balance
    graph_coloring_get_groups
-   radix_sort_pairs
-   runlength_encode
-   segmented_sort_pairs
+
+Misc
+----
+
+.. autosummary::
+   :nosignatures:
+   :toctree: _generated
+
+   create_warp_function
--- warp/utils.py
+++ warp/utils.py
@@ -17,18 +17,32 @@
 
 # isort: skip_file
 
+# category: Array Operations
+
 from warp._src.utils import array_cast as array_cast
 from warp._src.utils import array_inner as array_inner
 from warp._src.utils import array_scan as array_scan
 from warp._src.utils import array_sum as array_sum
-from warp._src.utils import create_warp_function as create_warp_function
+
+
+# category: Sorting
+
 from warp._src.utils import radix_sort_pairs as radix_sort_pairs
 from warp._src.utils import runlength_encode as runlength_encode
 from warp._src.utils import segmented_sort_pairs as segmented_sort_pairs
+
+
+# category: Graph Coloring
+
 from warp._src.coloring import GraphColoringAlgorithm as GraphColoringAlgorithm
 from warp._src.coloring import graph_coloring_assign as graph_coloring_assign
 from warp._src.coloring import graph_coloring_balance as graph_coloring_balance
 from warp._src.coloring import graph_coloring_get_groups as graph_coloring_get_groups
+
+
+# category: Misc
+
+from warp._src.utils import create_warp_function as create_warp_function
 
 
 # TODO: Remove after cleaning up the public API.
